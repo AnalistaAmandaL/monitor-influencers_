b8270e1e21b4ebc3c0f051504ec2a3ac39d61893
AnalistaAmandaL <amanda.lnevesgps@gmail.com>
2025-08-19 16:00:58 -0300

    Add files via upload

--- install-chrome.sh
+++ install-chrome.sh
@@ -2,25 +2,17 @@
 
 echo "--- Instalando o Google Chrome e dependências ---"
 
-# Atualiza a lista de pacotes
 sudo apt-get update
-
-# Instala dependências e utilitários
 sudo apt-get install -y wget gnupg
 
-# Baixa e adiciona a chave GPG do repositório do Google
+# Adiciona a chave GPG do repositório do Google
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 
-# Adiciona o repositório do Chrome às fontes de software
+# Adiciona o repositório do Chrome
 echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
 
-# Atualiza a lista de pacotes novamente para incluir o novo repositório
+# Atualiza e instala o Chrome
 sudo apt-get update
-
-# Instala o Google Chrome
 sudo apt-get install -y google-chrome-stable
-
-# Corrige permissões e outras dependências
-sudo chmod +x /usr/bin/google-chrome
 
 echo "--- Google Chrome instalado com sucesso ---"
